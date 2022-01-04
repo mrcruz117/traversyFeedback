52974ff512f148e95829047dee06b96fd1fd6327
mrcruz117 <mrcruz1989@gmail.com>
2022-01-04 13:44:44 -0500

    delete functionality complete

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
-
+import { FaTimes } from "react-icons/fa";
 import Card from "./shared/Card";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
